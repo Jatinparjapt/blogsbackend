f55b9e744d65ec335b6f018255cf31f434cf1ac5
Jatin kumar <jatin3961jk@gmail.com>
2025-07-30 22:44:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
-
+app.get("/" , (req , res)=>{
+    res.status(200).json({data: "home page " })
+})
 app.listen(PORT , ()=>{
     console.log("server started")
 })
